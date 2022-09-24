d14f915b57340547b3b8953c256c3817420d8d35
Alexander Getmansky <alex@getsky.games>
2022-09-24 19:59:15 +0300

    feat (camera): Add default camera

---
+++ Assets/Features/Sector/Camera/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Features.Sector.Camera
+{
+    public class CameraController : MonoBehaviour
+    {
+        private Vector3 _startPosition;
+        private UnityEngine.Camera _camera;
+
+        private void Start()
+        {
+            _camera = GetComponent<UnityEngine.Camera>();
+        }
+
+        private void Update()
+        {
+            var mousePosition = Input.mousePosition;
+            mousePosition.z = 10.0f;
+
+            if (Input.GetMouseButtonDown(1)) _startPosition = _camera.ScreenToWorldPoint(mousePosition);
+            else if (Input.GetMouseButton(1))
+            {
+                var position = transform.position;
+                transform.position = new Vector3(
+                    position.x - _camera.ScreenToWorldPoint(mousePosition).x - _startPosition.x,
+                    position.y,
+                    position.z - _camera.ScreenToWorldPoint(mousePosition).z - _startPosition.z
+                );
+            }
+        }
+    }
+}
