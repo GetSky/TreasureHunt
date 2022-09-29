5e32d46e154f6325f546edc0a46a71e84072da0a
Alexander Getmansky <alex@getsky.games>
2022-09-30 01:54:13 +0300

    refactor (camera): Clean code (#39)

--- Assets/Features/Sector/Camera/CameraController.cs
+++ Assets/Features/Sector/Camera/CameraController.cs
@@ -5,12 +5,6 @@ namespace Features.Sector.Camera
     public class CameraController : MonoBehaviour
     {
         private Vector3 _startPosition;
-        private UnityEngine.Camera _camera;
-
-        private void Start()
-        {
-            _camera = GetComponent<UnityEngine.Camera>();
-        }
 
         private void Update()
         {
@@ -19,15 +13,18 @@ namespace Features.Sector.Camera
             if (Input.GetMouseButtonDown(1)) _startPosition = mousePosition;
             else if (Input.GetMouseButton(1))
             {
-                var position = transform.position;
-                transform.position = new Vector3(
-                    position.x - (mousePosition.x - _startPosition.x) / 100,
-                    position.y,
-                    position.z - (mousePosition.y - _startPosition.y) / 100
-                );
-
+                transform.position = CalculatePosition(mousePosition, transform.position);
                 _startPosition = mousePosition;
             }
+        }
+
+        private Vector3 CalculatePosition(Vector3 mouse, Vector3 currentPosition)
+        {
+            return new Vector3(
+                currentPosition.x - (mouse.x - _startPosition.x) / 100,
+                currentPosition.y,
+                currentPosition.z - (mouse.y - _startPosition.y) / 100
+            );
         }
     }
 }
