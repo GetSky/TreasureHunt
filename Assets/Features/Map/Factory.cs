2ca9fd5aa661f35b48ae18b110d8e0eda35b7928
Alexander Getmansky <alex@getsky.games>
2022-10-06 20:43:17 +0300

    feat (map): Added map factory (#45)

---
+++ Assets/Features/Map/Factory.cs
@@ -0,0 +1,27 @@
+﻿using Features.Map.Repository;
+using Zenject;
+
+namespace Features.Map
+{
+    public class Factory : PlaceholderFactory<Map>
+    {
+    }
+
+    public class MapFactory : IFactory<Map>
+    {
+        private readonly IMapFlasher _mapFlasher;
+
+        public MapFactory(IMapFlasher mapFlasher)
+        {
+            _mapFlasher = mapFlasher;
+        }
+
+        public Map Create()
+        {
+            var entity = new Map("map");
+            _mapFlasher.Save(entity);
+
+            return entity;
+        }
+    }
+}
