0cd591efee350549855b85879030613861af77b2
Alexander Getmansky <alex@getsky.games>
2022-10-09 15:32:32 +0300

    refactor: Moved initialization to features

---
+++ Assets/Features/Map/MapInstaller.cs
@@ -0,0 +1,30 @@
+using Features.Map.Handler;
+using Features.Map.Repository;
+using UnityEngine;
+using Zenject;
+
+namespace Features.Map
+{
+    public class MapInstaller : Installer<GameObject, MapInstaller>
+    {
+        private readonly GameObject _groundPrefab;
+
+        public MapInstaller(GameObject groundPrefab)
+        {
+            _groundPrefab = groundPrefab;
+        }
+
+        public override void InstallBindings()
+        {
+            Container.BindFactory<Map, Features.Map.Factory>().FromFactory<MapFactory>();
+            Container.Bind<MapProducer>().AsTransient().WithArguments(_groundPrefab).Lazy();
+            Container.Bind<IRestartMapHandler>().To<RestartMapHandler>().AsSingle().Lazy();
+
+            Container
+                .Bind(typeof(IMapFlasher), typeof(IMapRepository))
+                .To<MemoryMapRepository>()
+                .AsSingle()
+                .NonLazy();
+        }
+    }
+}
