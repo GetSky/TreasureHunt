f397d7ad007dee64546de6bca36a2aa7b294ca48
Alexander Getmansky <alex@getsky.games>
2022-10-21 11:06:57 +0300

    feat (end-menu): Added handler reload map handler. (#75)

---
+++ Assets/Features/EndGameMenu/Handler/ReloadMapHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace Features.EndGameMenu.Handler
+{
+    public class ReloadMapHandler : IReloadMapHandler
+    {
+        private readonly IDeactivateHandler _deactivateHandler;
+        private readonly Map.Handler.IReloadMapHandler _mapHandler;
+
+        public ReloadMapHandler(IDeactivateHandler deactivateHandler, Map.Handler.IReloadMapHandler reloadMapHandler)
+        {
+            _deactivateHandler = deactivateHandler;
+            _mapHandler = reloadMapHandler;
+        }
+
+        public void Invoke(ReloadMapCommand command)
+        {
+            _deactivateHandler.Invoke(new DeactivateCommand());
+            _mapHandler.Invoke(new Map.Handler.ReloadMapCommand());
+        }
+    }
+}
