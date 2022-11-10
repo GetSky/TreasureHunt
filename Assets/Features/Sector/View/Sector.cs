884c1efc390b6449f1f0a55c0cf509c95fd9f84d
Alexander Getmansky <alex@getsky.games>
2022-11-10 22:14:19 +0300

    refactor (sector): Moved ground activation in a new class (#108)
    refactor (sector): Moved ground activation in a new class

--- Assets/Features/Sector/View/Sector.cs
+++ Assets/Features/Sector/View/Sector.cs
@@ -16,15 +16,6 @@ namespace Features.Sector.View
         [SerializeField] private float _animationDelay = 2.0f;
         [SerializeField] private CardState[] _states;
 
-        [SerializeField] private GameObject _centerTop;
-        [SerializeField] private GameObject _centerBottom;
-        [SerializeField] private GameObject _centerLeft;
-        [SerializeField] private GameObject _downLeft;
-        [SerializeField] private GameObject _topLeft;
-        [SerializeField] private GameObject _centerRight;
-        [SerializeField] private GameObject _topRight;
-        [SerializeField] private GameObject _bottomRight;
-
         private IInputSectorControl _input;
         private ISectorOpenHandler _openHandler;
         private Material _material;
@@ -91,44 +82,6 @@ namespace Features.Sector.View
             if (_isOpened) return;
             _material.DOKill();
             _material.DOColor(_shitColor, _animationDuration);
-        }
-
-        public void Start()
-        {
-            RaycastHit hit;
-
-            if (!Physics.Raycast(transform.position, Vector3.forward, out hit,
-                    1.5f))
-            {
-                _centerTop.SetActive(true);
-            }
-            
-            RaycastHit hit2;
-            if (!Physics.Raycast(transform.position, -1*Vector3.forward, out hit2,
-                    1.5f))
-            {
-                _centerBottom.SetActive(true);
-            } 
-            
-            RaycastHit hit3;
-            if (!Physics.Raycast(transform.position, Vector3.left, out hit3,
-                    1.5f))
-            {
-                _centerLeft.SetActive(true);
-            }
-            
-            if (hit2.collider == null && hit3.collider == null) _downLeft.SetActive(true);
-            if (hit.collider == null && hit3.collider == null) _topLeft.SetActive(true);
-            
-            RaycastHit hit4;
-            if (!Physics.Raycast(transform.position, Vector3.right, out hit4,
-                    1.5f))
-            {
-                _centerRight.SetActive(true);
-            }
-            
-            if (hit2.collider == null && hit4.collider == null) _bottomRight.SetActive(true);
-            if (hit.collider == null && hit4.collider == null) _topRight.SetActive(true);
         }
     }
 }
