f8a1acad7e3a1f4c97d08b67dd6445aeae5aed69
Alexander Getmansky <alex@getsky.games>
2022-11-12 13:54:53 +0300

    refactor (sector): Cleaned up code

--- Assets/Features/Sector/View/GroundCornerActivator.cs
+++ Assets/Features/Sector/View/GroundCornerActivator.cs
@@ -12,7 +12,7 @@ namespace Features.Sector.View
         [SerializeField] private GameObject _centerRight;
         [SerializeField] private GameObject _topRight;
         [SerializeField] private GameObject _bottomRight;
-        
+
         public void Start()
         {
             RaycastHit hit;
@@ -22,31 +22,31 @@ namespace Features.Sector.View
             {
                 _centerTop.SetActive(true);
             }
-            
+
             RaycastHit hit2;
-            if (!Physics.Raycast(transform.position, -1*Vector3.forward, out hit2,
+            if (!Physics.Raycast(transform.position, -1 * Vector3.forward, out hit2,
                     1.5f))
             {
                 _centerBottom.SetActive(true);
-            } 
-            
+            }
+
             RaycastHit hit3;
             if (!Physics.Raycast(transform.position, Vector3.left, out hit3,
                     1.5f))
             {
                 _centerLeft.SetActive(true);
             }
-            
+
             if (hit2.collider == null && hit3.collider == null) _downLeft.SetActive(true);
             if (hit.collider == null && hit3.collider == null) _topLeft.SetActive(true);
-            
+
             RaycastHit hit4;
             if (!Physics.Raycast(transform.position, Vector3.right, out hit4,
                     1.5f))
             {
                 _centerRight.SetActive(true);
             }
-            
+
             if (hit2.collider == null && hit4.collider == null) _bottomRight.SetActive(true);
             if (hit.collider == null && hit4.collider == null) _topRight.SetActive(true);
         }
