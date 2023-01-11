ac02082b5d6837b9ac82aaa7e15bae391c495662
Alexander Getmansky <alex@getsky.games>
2023-01-11 22:14:00 +0300

    feat (player): Added installer and initializer

---
+++ Assets/Features/Player/PlayerInstaller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Features.Player
+{
+    public class PlayerInstaller : Installer<GameObject, PlayerInstaller>
+    {
+        private readonly GameObject _coinsCounterPrefab;
+
+        public PlayerInstaller(GameObject coinsCounterPrefab)
+        {
+            _coinsCounterPrefab = coinsCounterPrefab;
+        }
+
+        public override void InstallBindings()
+        {
+            Container.Bind<IInitializable>().To<Initializer>().AsSingle().WithArguments(_coinsCounterPrefab);
+        }
+    }
+}
