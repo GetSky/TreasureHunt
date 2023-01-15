188d3f270dea4061e0c30207798c8e95b4b104cd
Alexander Getmansky <alex@getsky.games>
2023-01-15 21:50:30 +0300

    feat (player): Added blank classes

---
+++ Assets/Features/Player/Factory.cs
@@ -0,0 +1,29 @@
+﻿using Features.Player.Repository;
+using UnityEngine;
+using Zenject;
+
+namespace Features.Player
+{
+    public class Factory
+    {
+        private readonly DiContainer _container;
+        private readonly IPlayerContext _context;
+        private readonly GameObject _coinsCounterPrefab;
+        
+        public Factory(DiContainer container, IPlayerContext context, GameObject coinsCounterPrefab)
+        {
+            _container = container;
+            _context = context;
+            _coinsCounterPrefab = coinsCounterPrefab;
+        }
+
+
+        public Entity.Player Create()
+        {
+            var entity = new Entity.Player();
+            _context.Save(entity);
+
+            return entity;
+        }
+    }
+}
---
+++ Assets/Features/Player/Repository/IPlayerContext.cs
@@ -0,0 +1,7 @@
+﻿namespace Features.Player.Repository
+{
+    public interface IPlayerContext
+    {
+        public void Save(Entity.Player player);
+    }
+}
