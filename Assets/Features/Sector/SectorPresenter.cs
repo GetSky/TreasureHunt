3e99c62644c29c1e318776c738f40a11040bcf44
Alexander Getmansky <alex@getsky.games>
2023-05-13 01:59:21 +0300

    feat (sector): added experiment logic for create sector with sub contrainer

---
+++ Assets/Features/Sector/SectorPresenter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Features.Sector
+{
+    public class SectorPresenter
+    {
+        public event Action<bool> OnChangedHighlight;
+
+        public void ChangeHighlight(bool isHighlight)
+        {
+            OnChangedHighlight?.Invoke(isHighlight);
+        }
+    }
+}
---
+++ Assets/Features/Sector/SectorView.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Features.Sector
+{
+    public class SectorView : MonoBehaviour
+    {
+        private SectorPresenter _presenter;
+
+        [Inject]
+        public void Construct(SectorPresenter presenter)
+        {
+            _presenter = presenter;
+            _presenter.OnChangedHighlight += Highlight;
+        }
+
+        private void Highlight(bool isHighlight)
+        {
+            Debug.Log(gameObject.name + ": " + isHighlight);
+        }
+    }
+}
