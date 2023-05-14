8bf1257faa46c033efc634ff8107d5949ef5f3ef
Alexander Getmansky <alex@getsky.games>
2023-05-14 18:05:35 +0300

    refactor (sector): changed presenter

--- Assets/Features/Sector/SectorTick.cs
+++ Assets/Features/Sector/SectorTick.cs
@@ -1,5 +1,4 @@
-﻿using Features.Sector.Domain;
-using Features.Sector.UseCases.CreateSector;
+﻿using Features.Sector.UseCases.CreateSector;
 using UnityEngine;
 using Zenject;
 
@@ -8,26 +7,17 @@ namespace Features.Sector
     public class SectorTick : ITickable
     {
         private readonly Gateway _gateway;
-        private readonly ISectorRepository _repository;
 
-        public SectorTick(Gateway gateway, ISectorRepository repository)
+        public SectorTick(Gateway gateway)
         {
             _gateway = gateway;
-            _repository = repository;
         }
 
         public void Tick()
         {
             var random = new System.Random();
             if (Input.GetKeyDown(KeyCode.P))
-                _gateway.Schedule(new CreateSectorCommand(random.Next(0, 10), random.Next(0, 10), "card"));
-            if (!Input.GetKeyDown(KeyCode.Space)) return;
-
-            var sector = _repository.FindById("333");
-            if (sector is null) return;
-
-            if (sector.IsHighlight) sector.Unhighlight();
-            else sector.Highlight();
+                _gateway.Schedule(new CreateSectorCommand(random.Next(-10, 10), random.Next(-10, 10), "card"));
         }
     }
 }
