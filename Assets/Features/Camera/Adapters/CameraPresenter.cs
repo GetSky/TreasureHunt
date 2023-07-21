9e5474f43d8c64b66dde2634f8a1d337f95a6113
Alexander Getmansky <alex@getsky.games>
2023-07-21 20:19:14 +0300

    refactor (camera): cleaned code

--- Assets/Features/Camera/Adapters/CameraPresenter.cs
+++ Assets/Features/Camera/Adapters/CameraPresenter.cs
@@ -2,7 +2,7 @@
 {
     public class CameraPresenter : ICameraPresenter
     {
-        public event ICameraPresenter.OnLookedAtHandler OnLookedAt;
+        public event ICameraPresenter.LookedAtHandler OnLookedAt;
 
         public void LookAt(float x, float z, bool isImmediately) => OnLookedAt?.Invoke(x, z, isImmediately);
     }
